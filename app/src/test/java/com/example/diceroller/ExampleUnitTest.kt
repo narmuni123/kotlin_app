ff43d03178faeb5a8f0d0e31698e297cd3a99564
Munikiran Narpareddi <narmuni123@gmail.com>
2023-07-25 08:21:56 +0530

    Added some unit tsts
    Unit test on randomn number generator added.

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_function() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of the result is not between 1 and 6", rollResult in 1..6)
     }
 }
